0010cbdbe80916d623ae6e8bede8d5ff95354403
HimanshuKumarSah <himanshu.sah2004@gmail.com>
2026-01-12 17:13:23 +0530

    Added token usage display after each response.

--- gemini_bot.py
+++ gemini_bot.py
@@ -11,7 +11,7 @@ client = genai.Client(api_key=api_key)
 model_id = "gemini-2.5-flash"
 
 system_prompt = "You're a chatbot meant to give quick and 2 lines short responses."
-sys_temp = 0.5
+sys_temp = 0.4
 
 def start_chat():
     chat = client.chats.create(
@@ -29,7 +29,12 @@ def start_chat():
         try:
             stream = True
             response = chat.send_message(user_input)
+            usage = response.usage_metadata
             print(f"Bot:, {response.text}\n")
+
+            print(f"Input token: {usage.prompt_token_count}")
+            print(f"Output token: {usage.completion_token_count}")
+            print(f"Total tokens used: {usage.total_token_count}")
 
         except Exception as e:
             print(f"Error: {e}")
